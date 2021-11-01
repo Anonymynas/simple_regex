248c4476c7102c209ad47c9d7f17305e3e327da7
Nas <nasrullah.azali99@gmail.com>
2021-11-01 12:26:19 +0800

    This is simple python regex code

---
+++ regex.py
@@ -0,0 +1,10 @@
+import re
+
+pattern = "[a-zA-Z0-9]+@[a-zA-Z]+\.(com|edu|net)"
+
+user_input = input("Enter Email: ")
+
+if(re.search(pattern, user_input)):
+    print('valid email')
+else:
+    print('invalid email')
